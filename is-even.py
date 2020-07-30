8b221a6802faf6cfdc18ea609074af24d4bbb749
AlbertoCaballero <albertocaballero80@outlook.com>
2020-07-30 12:46:51 -0500

    More stuff

---
+++ is-even.py
@@ -0,0 +1,11 @@
+print("Check if number is even")
+number = int(input("> "))
+
+def isEven(n):
+    if (n%2) == 1:
+        return "None"
+    else:
+        return "Even"
+
+print(isEven(number))
+
