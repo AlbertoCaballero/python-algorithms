1ff9f1f664503b2b68c1b4daa8fb0be84f52b829
AlbertoCaballero <albertocaballero80@outlook.com>
2020-09-09 16:08:05 -0500

    GTK GUI in Python

---
+++ gtkgui.py
@@ -0,0 +1,25 @@
+# Load Gtk
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+# When the application is launched…
+
+
+def on_activate(app):
+    # … create a new window…
+    win = Gtk.ApplicationWindow(application=app)
+    # … with a button in it…
+    btn = Gtk.Button(label='Hello, World!')
+    # … which closes the window when clicked
+    btn.connect('clicked', lambda x: win.close())
+    win.add(btn)
+    win.show_all()
+
+# Create a new application
+app = Gtk.Application(
+application_id='com.example.GtkApplication')
+app.connect('activate', on_activate)
+
+# Run the application
+app.run(None)
