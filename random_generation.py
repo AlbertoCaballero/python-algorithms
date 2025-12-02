01ccbef7ee60128556bf852c9b0aa292d2679979
AlbertoCaballero <albertoCaballero80@outlook.com>
2025-12-01 22:33:41 -0600

    Random functions

---
+++ random_generation.py
@@ -0,0 +1,16 @@
+import random
+
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+random_floating = random.random()
+print(random_floating)
+
+random_uniform = random.uniform(1, 10)
+print(random_uniform)
+
+def coin_toss ():
+    print("HEADS") if random.randint(0, 1) == 0 else print("TAILS")
+
+coin_toss()
+
