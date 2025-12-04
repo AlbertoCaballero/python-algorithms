83f174fcee42ce4e45e4bd7d32f2fdc282df6fc2
AlbertoCaballero <albertoCaballero80@outlook.com>
2025-12-04 02:05:10 -0600

    Reeborgs hurdles 4

--- reeborg-hurdle.py
+++ reeborg-hurdle.py
@@ -6,16 +6,19 @@ def turn_right():
 
 def jump():
     turn_left()
-    move()
+    while wall_on_right():
+        move()
     turn_right()
     move()
     turn_right()
-    move()
+    while front_is_clear():
+        move()
     turn_left()
-
+    
 while not at_goal():
     if front_is_clear():
         move()
     elif wall_in_front():
         jump()
+
 
