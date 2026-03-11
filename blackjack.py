416918e91b99bb0de6aeb87acb2186e520ee0036
AlbertoCaballero <albertoCaballero80@outlook.com>
2026-03-11 02:10:45 -0600

    Black jack game rules

--- blackjack.py
+++ blackjack.py
@@ -18,24 +18,82 @@
 # If dealer value less than 17, takes another card
 #
 
-cards = [ "A", "2", "3", "4", "5", "6", "7", "8", "9", "10," "J", "K", "Q" ]
-cards_values = {
-    "A": 11,
-    "2": 2,
-    "3": 3,
-    "4": 4,
-    "5": 5,
-    "6": 6,
-    "7": 7,
-    "8": 8,
-    "9": 9,
-    "10": 10,
-    "J": 10,
-    "K": 10,
-    "Q": 10,
-}
+import random
 
-def draw_game():
-    print(cards_values["A"])
+cards = [ "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "K", "Q", ]
+cards_values = { "A": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 10, "K": 10, "Q": 10, }
 
-draw_game()
+player = []
+dealer = []
+
+def draw_random_card():
+    return cards[random.randint(0, len(cards)-1)]
+
+def count_total(game):
+    # list(map(lambda x: cards_values[x], game))
+    return sum([cards_values[card] for card in game])
+
+def print_player(p):
+    print(f"\nPlayer: {[f'{x}' for x in p]}")
+    print(f"Total: {count_total(p)}\n")
+
+def print_dealer(d, s):
+    if not s:
+        print(f"\nDealer: [{d[0] if s else "?"}, {d[1]}]")
+        print(f"Total: {count_total(d) if s else "?"}\n")
+    elif s:
+        print(f"\nDealer: {d}")
+        print(f"Total: {count_total(d)}\n")
+
+def win_or_lose(d, p):
+    d_total = count_total(d)
+    p_total = count_total(p)
+
+    if p_total > 21:
+        print("===LOSE===")
+    elif d_total == p_total:
+        print("===DRAW===")
+    elif d_total > p_total:
+        print("===LOSE===")
+    elif d_total < p_total:
+        print("===WIN===")
+
+
+def game_loop():
+    play = ""
+    while True:
+        print("==============================BLACK JACK==============================")
+        play = input("You want to play Black Jack? Y/N\n")
+        player = []
+        dealer = []
+        if play == "Y":
+            print("DEAL!\n")
+
+            player.append(draw_random_card())
+            player.append(draw_random_card())
+
+            dealer.append(draw_random_card())
+            dealer.append(draw_random_card())
+
+            print_dealer(dealer, False)
+            print_player(player)
+
+            more = input("Card or Fold? C/F\n")
+            if more == "C":
+                player.append(draw_random_card())
+
+            if count_total(dealer) < 17:
+                dealer.append(draw_random_card())
+
+            print_dealer(dealer, True)
+            print_player(player)
+            win_or_lose(dealer, player)
+
+        elif play == "N":
+            break
+        else:
+            print("No valid option.\n")
+
+    print("\nBYE")
+
+game_loop()
